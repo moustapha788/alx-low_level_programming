9dda96a9ef86badce159585e72a073d16c32557e
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-09-09 18:21:37 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c1, c2;
+
+	c1 = 48;
+	c2 = 97;
+	/* a program that prints the alphabet in lowercase, followed by a new line. */
+	while (c1 <= 57)
+	{
+		putchar(c1);
+		c1++;
+	}
+	while (c2 <= 101)
+	{
+		putchar(c2);
+		c2++;
+	}
+	putchar('\n');
+	return (0);
+}
