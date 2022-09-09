ebe528114c6a34aac3c7ed00825335e1ddf93cda
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-09-09 17:21:17 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
