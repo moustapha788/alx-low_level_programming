974896f9695314fc5566ab4426bced1c5758b9c7
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-09-13 23:51:46 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');/*_*/
+	_putchar('p');/*p=112*/
+	_putchar('u');/*u=117*/
+	_putchar('t');/*t=116*/
+	_putchar('c');/*c=99*/
+	_putchar('h');/*h=104*/
+	_putchar('a');/*a=65*/
+	_putchar('r');/*r=114*/
+	_putchar('\n');/*naw line*/
+
+	return (0);
+}
