7fdedc3fb8d228a70f9552e76bf08e1cca48043b
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-09-14 19:56:54 +0000

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * naturalNumbers1024 - computes and prints the sum of
+ * all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ *
+ *@n: number
+ * Return:   void
+ */
+void naturalNumbers1024(int n)
+{
+
+	int sum = 0, i = 1;
+
+	while (i < n)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+
+		i++;
+	}
+	printf("%d\n", sum);
+}
