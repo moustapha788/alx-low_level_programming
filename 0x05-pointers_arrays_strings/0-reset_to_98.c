1d4edfe3ce6c1edab2c3702659659f5425254f5f
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-09-19 14:01:12 +0000

    creting files of mandatory tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/* more headers goes there */
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 execpt 2 and 4,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+
+		_putchar(i);
+	}
+	_putchar('\n');
+}
